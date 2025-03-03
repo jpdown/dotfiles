9a5224f4409905fb7870123d9beb24414c22c091
Jaden Down <24631328+jpdown@users.noreply.github.com>
2025-03-03 15:22:03 -0600

    some updates

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    function cdg
+        set repo (eza -D ~/git | fzf --ansi)
+        if test -n "$repo"
+            cd ~/git/$repo
+        end
+    end
+
 end
+
+# Created by `pipx` on 2025-03-03 21:21:06
+set PATH $PATH /home/jaden/.local/bin
