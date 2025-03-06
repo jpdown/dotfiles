0983f73b9afc47ad42885431f7079d99057067da
Jaden Down <24631328+jpdown@users.noreply.github.com>
2025-03-06 10:08:40 -0600

    install.sh removes existing symlinks

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+# these rm commands will fail if they are folders and not symlinks
+
+rm ~/.config/nvim
 ln -s "$(pwd)/nvim/" ~/.config/nvim
+rm ~/.config/ghostty
 ln -s "$(pwd)/ghostty/" ~/.config/ghostty
+rm ~/.config/fish
 ln -s "$(pwd)/fish/" ~/.config/fish
