2589eec49453968c6da8a98900d5439d8981a372
Jaden Down <24631328+jpdown@users.noreply.github.com>
2025-03-31 14:54:25 -0500

    switch to delta as git diffing tool

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ rm ~/.config/ghostty
 ln -s "$(pwd)/ghostty/" ~/.config/ghostty
 rm ~/.config/fish
 ln -s "$(pwd)/fish/" ~/.config/fish
+rm ~/.config/lazygit
+ln -s "$(pwd)/lazygit/" ~/.config/lazygit
